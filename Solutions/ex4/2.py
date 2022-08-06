2d2b0707216c75628358a9e0f509d95c7e87adee
efi <erfanghorbanee@gmail.com>
2022-08-06 10:06:49 +0430

    Style improved by black

--- Solutions/ex4/2.py
+++ Solutions/ex4/2.py
@@ -1,38 +1,31 @@
 import argparse
 
-my_parser = argparse.ArgumentParser(description='Simple solution for ex4.',
-                                    epilog='Enjoy the program! :)')
+my_parser = argparse.ArgumentParser(
+    description="Simple solution for ex4.", epilog="Enjoy the program! :)"
+)
 
 # positional args that are required and the order matters!
-my_parser.add_argument('fname', help='Your first name')
+my_parser.add_argument("fname", help="Your first name")
 
-my_parser.add_argument('lname', help='Your last name')
+my_parser.add_argument("lname", help="Your last name")
 
-my_parser.add_argument('country', help='Your country')
+my_parser.add_argument("country", help="Your country")
 
 
 # optional args that take a value
-my_parser.add_argument('-a', help='option a')
+my_parser.add_argument("-a", help="option a")
 
-my_parser.add_argument('-b', help='option b')
+my_parser.add_argument("-b", help="option b")
 
-my_parser.add_argument('-c', help='option c')
+my_parser.add_argument("-c", help="option c")
 
 
 # flags
-my_parser.add_argument('-v',
-                       '--verbose',
-                       action='store_true',
-                       help='flag v')
+my_parser.add_argument("-v", "--verbose", action="store_true", help="flag v")
 
-my_parser.add_argument('-d',
-                       action='store_true',
-                       help='flag d')
+my_parser.add_argument("-d", action="store_true", help="flag d")
 
-my_parser.add_argument('-e',
-                       action='store_true',
-                       help='flag e')
-
+my_parser.add_argument("-e", action="store_true", help="flag e")
 
 
 # Execute parse_args()
